95091447a81b8c6d5a782ac726f58ff6b9c31b16
dimagidan <dmyung@dimagi.com>
2009-11-20 13:06:12 -0500

    adding whole lots of stuff reversion for 1.1 and 1.0 versioning
    django userprofiles is refactored so now it works in the rapidsms seutp

---
+++ apps/ashand/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from reversion.admin import VersionAdmin
+
+from django.contrib.auth.models import User
+from models import AshandProfile
+
+
+try:
+    admin.site.unregister(User)
+except:
+    pass
+
+class UserRevision(VersionAdmin):
+    list_display=('id','first_name','last_name','email')
+
+admin.site.register(User, UserRevision)
+
+class AshandProfileAdmin(VersionAdmin):
+    list_display = ('id','firstname','surname', 'user')
+    list_filter = []    
+
+admin.site.register(AshandProfile,AshandProfileAdmin)
---
+++ apps/reversion_dj1.1/__init__.py
@@ -0,0 +1,16 @@
+"""
+Transactional version control for Django models.
+
+Project sponsored by Etianen.com
+
+<http://www.etianen.com/>
+"""
+print "reversion init"
+from reversion.revisions import revision
+
+
+# Legacy registration methods, now delegating to the revision object.
+register = revision.register
+is_registered = revision.is_registered
+unregister = revision.unregister
+
