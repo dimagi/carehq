3311bbab482945c68d00df46f34812baeb12bc5f
dimagidan <dmyung@dimagi.com>
2009-12-15 14:26:10 -0500

    fixing up the wsgi script for apache to not use the rapidsms settings

--- utilities/deployment/ashand_wsgi.py
+++ utilities/deployment/ashand_wsgi.py
@@ -6,22 +6,17 @@ filedir = os.path.dirname(__file__)
 
 rootpath = os.path.join(filedir, "..", "..") 
 sys.path.append(os.path.join(rootpath))
+sys.path.append(os.path.join(rootpath,'..'))
 sys.path.append(os.path.join(rootpath,'apps'))
 sys.path.append(os.path.join(rootpath,'lib'))
+sys.path.append(os.path.join(rootpath,'rapidsms','apps'))
 
 #rapidsms lib stuff
-sys.path.append(os.path.join(rootpath,'rapidsms'))
-sys.path.append(os.path.join(rootpath,'rapidsms','apps'))
 sys.path.append(os.path.join(rootpath,'rapidsms','lib'))
 sys.path.append(os.path.join(rootpath,'rapidsms','lib','rapidsms'))
 sys.path.append(os.path.join(rootpath,'rapidsms','lib','rapidsms','webui'))
 
-
-os.environ['RAPIDSMS_INI'] = os.path.join(rootpath,'local.ini')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'rapidsms.webui.settings'
-os.environ["RAPIDSMS_HOME"] = rootpath
-
-from rapidsms.webui import settings
+os.environ['DJANGO_SETTINGS_MODULE'] = 'ashand.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
