e604905ccb0472a74ff8c4d62e2b93adb0de4171
Dan Myung <dmyung@dimagi.com>
2010-08-10 02:31:19 -0400

    removing extraneous bootstrap script

--- utilities/bootstrap_database.py
+++
@@ -1,7 +0,0 @@
-from django.core.management import call_command
-call_command('loaddata', '0-caseaction.json')
-call_command('loaddata', '1-category.json')
-call_command('loaddata', '2-eventactivity.json')
-call_command('loaddata', '3-priority.json')
-call_command('loaddata', '4-status.json')
-call_command('loaddata', '5-gridcolumns.json')
