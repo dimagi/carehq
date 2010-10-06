e32c1879cbeb5ccf613d44502577e6c28dc0d8ff
root <root@ashand.com>
2010-10-06 12:43:59 -0400

    updating urls for wrapper app to match pact pilot

--- apps/pact-carehq/urls.py
+++ apps/pact-carehq/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 
 #(r'^projects/(?P<project_id>\d+)/?$', 'buildmanager.views.show_project'),
 urlpatterns = patterns ('',
-    (r'^caselist$', 'pact-carehq.views.get_caselist'),
+    (r'^provider/caselist$', 'pact-carehq.views.get_caselist'),
 )
