38499286cf3c865c63e997b361dc48f35e01bcd2
Dan Myung <dmyung@dimagi.com>
2010-11-03 15:12:07 -0400

    updating pact workflow to parse json of dots form upon submission

---
+++ pact_apps/dotsview/scripts/parsedotsdata.py
@@ -0,0 +1,24 @@
+from couchforms.models import XFormInstance
+from dotsview.formprocess import process_dots_json
+import simplejson
+
+def run():
+    docs = XFormInstance.view('pactcarehq/all_dots_forms').all()
+    for doc in docs:
+        try:
+            if not doc['form']['case']['update'].has_key('dots'):
+                print "don't got dots key"
+                continue
+
+            dots_json = doc['form']['case']['update']['dots']
+            if isinstance(dots_json, dict):
+                print "dictionary, skipping"
+                continue
+            elif isinstance(dots_json, str):
+                json_data = simplejson.loads(dots_json)
+                doc['form']['case']['update']['dots'] = json_data
+                doc.save()
+        except Exception, ex:
+            print "Error - dots key not present: version %s uiVersion %s" % (doc['form']['@version'], doc['form']['@uiVersion'])
+            print "Exception: %s" % (ex.message)
+
--- pact_apps/pactcarehq/scripts/export_dots.py
+++ pact_apps/pactcarehq/scripts/export_dots.py
@@ -1,13 +1,22 @@
 from dotsview.models import Observation
 from patient.models.djangomodels import Patient
+from couchforms.models import XFormInstance
+from datetime import datetime
 
 def run():
     observations = Observation.objects.all()
-    fields = ['observation_id', 'pt_pact_id', 'date','is_art', 'adherence','method','note','dose_number','total_doses']
+    fields = ['observation_id', 'observation_date', 'pt_pact_id', 'check_date','is_art', 'adherence','method','note','dose_number','total_doses', 'anchor_date']
     print ','.join(fields)
     for obs in observations:
         output = []
-        output.append(str(obs.id))
+        output.append(str(obs.doc_id))
+        doc = XFormInstance.view('pactcarehq/all_dots_forms', key=obs.doc_id).first()
+        if (doc):
+            output.append(doc['form']['encounter_date'].strftime('%Y-%m-%d'))
+        else:
+            output.append("no-encounter")
+            print "WTF"
+
         output.append(obs.patient.couchdoc['pact_id'])
         output.append(obs.date.strftime("%Y-%m-%d"))
         if obs.is_art:
@@ -22,6 +31,13 @@ def run():
             output.append('')
         output.append(repr(obs.dose_number))
         output.append(repr(obs.total_doses))
+
+        if doc:
+            anchor = doc['case']['update']['dots']['anchor']
+            anchor_date = datetime.strptime(anchor, "%d %b %Y %H:%M:%S %Z")
+            output.append(anchor_date.strftime('%Y-%m-%d'))
+        else:
+            output.append("no-anchor")
 
         #print output
         print ','.join(output)
