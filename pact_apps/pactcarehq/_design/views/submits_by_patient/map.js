ec9d9f1f8dc008f4c701bffd21709b63cfad2f24
Dan Myung <dmyung@dimagi.com>
2010-12-12 01:28:25 -0500

    adding new views for viewing submissions, by chw and by patient

---
+++ pact_apps/pactcarehq/_design/views/submits_by_patient/map.js
@@ -0,0 +1,7 @@
+function(doc) {
+    //Parse the encounter date string.
+    //note, that the dates are in 1 indexed months, so NO additions here.
+    if (doc.doc_type == "XFormInstance") {
+        emit(doc.form.pact_id, doc);
+    }
+}
