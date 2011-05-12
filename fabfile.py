0a1e1cb62960da089adc7a294c5292a1e7f374a5
Dan Myung <dmyung@dimagi.com>
2011-05-12 01:33:09 -0400

    updating fab file ot reflect final directory structure

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,8 @@
+import fabric
 from fabric.api import *
 from fabric.contrib import console
 from fabric import utils
+from fabric.contrib import files
 
 env.root = '/opt/carehq_project
 env.code_repo = 'git://github.com/dimagi/carehq.git
@@ -13,15 +15,16 @@ def _join(*args):
 
 
 def _setup_path():
-    env.virtualenv_root = _join(env.root, 'env/cchq_www')
-    env.code_root       = _join(env.root, 'src/commcare-hq')
-    env.project_root    = _join(env.root, 'src/commcare-hq')
+    env.virtualenv_root = '/home/pact/.virtualenvs/carehq'
+    env.src_root       = _join(env.root, 'src')
+    env.code_root       = _join(env.root, 'src/carehq')
+    env.project_root    = _join(env.root, 'src/carehq')
 
 def production():
     """ use production environment on remote host"""
-    env.code_branch = 'master'
-    env.sudo_user = 'cchqwww'
-    env.hosts = ['10.84.168.241']
+    env.code_branch = 'develop'
+    env.sudo_user = 'pact'
+    env.hosts = ['10.84.168.247']
     env.environment = 'production'
     env.user = prompt("Username: ", default=env.user)
     _setup_path()
@@ -46,7 +49,10 @@ def deploy():
     if env.environment == 'production':
         if not console.confirm('Are you sure you want to deploy production?', default=False):
             utils.abort('Production deployment aborted.')
-
+    
+    if files.exists(env.src_root) == False:
+        with cd(env.src_root):
+            sudo('git clone %(code_repo)s' % env, user=env.sudo_user)
     with cd(env.code_root):
         sudo('git checkout %(code_branch)s' % env, user=env.sudo_user)
         sudo('git pull', user=env.sudo_user)
@@ -62,10 +68,17 @@ def deploy():
 
 def service_restart():
     """ restart cchq_www service on remote host.  This will call a stop, reload the initctl to
-    have any config file updates be reloaded into intictl, then start cchqwww again.
+    have any config file updates be reloaded into intictl, then start carehq again.
     """
     require('root', provided_by=('staging', 'production'))
     with settings(sudo_user="root"):
-        sudo('stop cchq_www', user=env.sudo_user)
+        sudo('stop carehq_django', user=env.sudo_user)
+        sudo('stop carehq_formsplayer', user=env.sudo_user)
+        sudo('stop carehq_celery', user=env.sudo_user)
+
         sudo('initctl reload-configuration', user=env.sudo_user)
-        sudo('start cchq_www', user=env.sudo_user)
+
+        sudo('start carehq_django', user=env.sudo_user)
+        sudo('start carehq_celery', user=env.sudo_user)
+        sudo('start carehq_formsplayer', user=env.sudo_user)
+
