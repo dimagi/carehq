aba3ef861a61ebc13ada225bfb0e7aff29dd63cd
Dan Myung <dmyung@dimagi.com>
2011-05-16 14:56:00 -0400

    updating test to use pact_data dict for xforminstances

--- pact_apps/dotsview/tests/submit.py
+++ pact_apps/dotsview/tests/submit.py
@@ -11,7 +11,6 @@ class SubmitParsingTests(TestCase):
         pass
 
     def testSubmitAndVerifyParse(self):
-        print "in testSubmitAndVerifyParse"
         sample_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'sampledata')
         files = os.listdir(sample_dir)
         for f in files:
@@ -20,15 +19,13 @@ class SubmitParsingTests(TestCase):
             fin.close()
 
             start_time = datetime.utcnow()
-            print "Begin sample submission"
             doc = post_xform_to_couch(xml_string)
             delta_post =  datetime.utcnow() - start_time
-            print "Submission posted: %d ms" % (ms_from_timedelta(delta_post))
             #xform_saved.send(sender="post", xform=doc) #ghetto way of signalling a submission signal
             #delta_signal = datetime.utcnow() - (start_time + delta_post)
             #print "Signal emitted: %d ms" % (ms_from_timedelta(delta_signal))
 
-            self.assertTrue(isinstance(doc['form']['case']['update']['dots'], dict))
+            self.assertTrue(isinstance(doc['pact_data']['dots'], dict))
 
 
         pass
