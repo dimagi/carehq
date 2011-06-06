e8f6f6dd955fc820a5b4ba26b49b1f0e1d9a32aa
dmyung <dmyung@dimagi.com>
2011-06-06 11:32:48 -0400

    updaitng requirements and the default settings for new dev. setup

--- settings_default.py
+++ settings_default.py
@@ -1,66 +1,151 @@
+import logging
+
+# Site-specific configuration settings for ASHand
+# Definitions of these settings can be found at
+# http://docs.djangoproject.com/en/dev/ref/settings/
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+import os
 from django.conf.urls.defaults import patterns, include
-# Unique secret key. Don't share this with anybody.
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ''
 
 
-#Local DB Settings
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+XFORMS_PLAYER_URL = "http://localhost:4444/"
+AUTH_PROFILE_MODULE = 'actorprofile.ClinicalUserProfile'
+
+#####
+#Shared Settings
+#####
+
+ADMINS = (
+    ('foo', 'foo@dimagi.com'),
+)
+
+import restkit
+restkit.set_logging(level=logging.ERROR)
+import couchdbkit
+couchdbkit.set_logging(level=logging.ERROR)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+filepath = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(filepath,'mediafiles')
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
+AUX_MEDIA_DIRS = { }
 
 # Extra site information.
 SITE_ID = 1
 SITE_ROOT = '/'
 DEBUG = True
 
-#cache settings
-CACHE_BACKEND = 'file:///tmp/django_cache'
-
-#Email settings
-EMAIL_HOST=''
-EMAIL_HOST_USER=''
-EMAIL_HOST_PASSWORD=''
+# Email setup
+# email settings: these ones are the custom hq ones
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD=""
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_PORT=587 #for gmail
 
-#Logging settings
-DJANGO_LOG_FILE = "/var/log/datahq/datahq.django.log"
-LOG_SIZE = 1000000
-LOG_LEVEL   = "DEBUG"
-LOG_FILE    = "/var/log/datahq/datahq.log"
-LOG_FORMAT  = "[%(name)s]: %(message)s"
-LOG_BACKUPS = 256 # number of logs to keep
+# Unique secret key. Don't share this with anybody.
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = ''
 
-#COUCHDB SETTINGS
+THUMBNAIL_DEBUG = True
+
+DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'couchdebugpanel.CouchDBLoggingPanel',
+    )
+
+
+
+LOGIN_TEMPLATE='pactregistration/login.html'
+LOGGEDOUT_TEMPLATE='pactregistration/logged_out.html'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'carehq',
+        'USER': 'carehq',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+    }
+}
+# Cache backend settings.
+CACHE_BACKEND = 'memcached://localhost:11211/'
+
+#PACT
+LOCAL_APPS = (
+                #dev, debug
+                'devserver',
+                'debug_toolbar',
+                'couchdebugpanel',
+
+                #core pact
+                'pactcarehq',
+                'keymaster',
+                'dotsview',
+                'pactpatient',
+                'webxforms',
+
+                #shine
+                'sorl.thumbnail',
+                'slidesview',
+
+
+)
 COUCH_SERVER_ROOT = 'localhost:5984'
-COUCH_USERNAME = 'admin'
+COUCH_USERNAME = ''
 COUCH_PASSWORD = ''
-COUCH_DATABASE_NAME = '' #your couchdb here.
-COUCH_DB_APPS = ['patient','couchforms','couchexport',] #other apps
+COUCH_DATABASE_NAME = 'carehq'
 
-#App Settings
-#tuple of apps you want to add to this carehq instance
-LOCAL_APPS = ()
+LOCAL_COUCHDB_APPS = [  'pactpatient', 'touchforms.formplayer', 'actorprofile','dotsview', 'pactcarehq', 'slidesview' ]
+
+#local settings does this to make this a lot easeir
 LOCAL_APP_URLS = patterns('',
-                          (r'', include('patient.urls'))
-                        )
+    (r'', include('pactcarehq.urls')),
+    (r'', include('pactpatient.urls')),
+    (r'', include('patient.urls')),
+    (r'', include('keymaster.urls')),
+    (r'', include('dotsview.urls')),
+    (r'', include('auditcare.urls')),
+    (r'', include('couchdebugpanel.urls')),
+    (r'', include('slidesview.urls')),
+    (r'^couchlog/', include('couchlog.urls')),
+    (r'^formplayer/', include('touchforms.formplayer.urls')),
+    (r'^webxforms/', include('webxforms.urls')),
+
+    #these are the static media for fomsplayer since it's not using the django staticfiles app
+    (r'^%s/formplayer/(?P<path>.*)$' % 'media', 'django.views.static.serve', {'document_root': os.path.join(filepath, 'submodules','touchforms','touchforms','formplayer','static') }),
+    (r'^%s/(?P<path>.*)$' % 'media', 'django.views.static.serve', {'document_root': MEDIA_ROOT}),
+)
 
 
-#DEV_APPS are the apps in which you care about for unit testing.
-DEV_APPS=['couchlog', 'couchforms','couchexport','patient','actors',]
-
+AUDIT_VIEWS = ['pactcarehq.views.post',]
 AUDIT_MODEL_SAVE = [
-                        'django.contrib.auth.models.User',
-                        'pactpatient.models.PactPatient',
-                        'dotsview.models.couchmodels.CObservationAddendum',
-                        'dotsview.models.couchmodels.CObservation',
-                        ]
+                    'django.contrib.auth.models.User',
+                    'patient.models.BasePatient',
+                    'dotsview.models.couchmodels.CObservationAddendum',
+                    'dotsview.models.couchmodels.CObservation',
+                    ]
+DEV_APPS=['couchlog','couchforms','couchexport','patient','actors','keymaster','pactcarehq','dotsview','auditcare', ]
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'auditcare.middleware.AuditMiddleware',
+    'dimagi.utils.threadlocals.ThreadLocals',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
 
