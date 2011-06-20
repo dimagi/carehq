577f06f906d0ec47b42fa28a9de41e9fc7ecb202
Cory Zue <czue@dimagi.com>
2011-06-20 16:22:38 -0400

    bugfix for ota restore

--- shine_apps/shineforms/models.py
+++ shine_apps/shineforms/models.py
@@ -10,6 +10,6 @@ class ShineUser(User):
         """
         For now Shine Users all get the same (full) case list.
         """
-        return [(case, case_previously_synced(case.get_id, last_sync))\
+        return [(case, not case_previously_synced(case.get_id, last_sync))\
                 for case in CommCareCase.view("shinepatient/cases_by_patient_id", include_docs=True).all()]
     
