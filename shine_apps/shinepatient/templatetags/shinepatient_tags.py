35d9361472c8200097eafcdd3ec86abaf257d9f4
Cory Zue <czue@dimagi.com>
2011-06-20 16:37:39 -0400

    add simple barcode view

--- shine_apps/shinepatient/templatetags/shinepatient_tags.py
+++ shine_apps/shinepatient/templatetags/shinepatient_tags.py
@@ -5,8 +5,27 @@ from casexml.apps.case.models import CommCareCase
 
 register = template.Library()
 
+MISSING_DEPENDECY = \
+"""Aw shucks, someone forgot to install the google chart library 
+on this machine and this feature needs it. To get it, run 
+easy_install pygooglechart.  Until you do that this won't work.
+"""
+
 @register.simple_tag
 def render_cases(patient):
     cases = CommCareCase.view("shinepatient/cases_by_patient_id", include_docs=True).all()
     return render_to_string("shinepatient/partials/itemlist.html", {"cases": cases})
 
+@register.simple_tag
+def render_barcode(barcode):
+    try:
+        from pygooglechart import QRChart
+    except ImportError:
+        raise Exception(MISSING_DEPENDECY)
+    HEIGHT = WIDTH = 150
+    code = QRChart(HEIGHT, WIDTH)
+    code.add_data(barcode)
+    return '<p style="font-weight: bold;">Scan Item Here: </p><img src="%(url)s" title="%(barcode)s" />' \
+            % {"url": code.get_url(), "barcode": barcode} 
+    
+    
