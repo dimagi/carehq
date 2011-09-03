90c528bda7f21dddee48f8faf4e0ddb3b61ca9d2
dmyung <dmyung@dimagi.com>
2011-09-03 22:47:38 +0200

    adding more documentation

--- services/pysocket.py
+++ services/pysocket.py
@@ -14,9 +14,11 @@ port = 9100
 #name, age, gender, oh yeah.
 #^FO50,50^B3N,N,100,Y,N^FD123456^FS
 #THIS IS THE QR CODE COMMAND
+#QR code  BQ,2,[<zoom/scale> 5 or 6, maybe 4, but 6 is the max for the 2x1],Q|M, Q is default, 
+#
 command = """
 ^XA
-^FO20,15^BQ,2,5^FDMA,%(barcode_data)s^FS
+^FO20,15^BQ,2,5,,^FDMA,%(barcode_data)s^FS
 ^FO175,20^A0,30,22^FD%(last_name)s,^FS
 ^FO175,50^A0,24,^FD%(first_name)s^FS
 ^FO175,100^ABN,16,^FDSex: %(gender)s Age: %(age)s^FS
@@ -37,7 +39,7 @@ lab_command = """
 
 #max_len=21 for surname
 #max_len=21 for firstname
-#save this to the printer
+#save this to the printer as an image for dispaly. SAVES PAPER
 #^ISR:EXERPROG.GRF,N
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -64,8 +66,6 @@ def flat_code():
     label_data['first_name']='Jonathan'
     s.send(lab_command % (label_data))
     s.close()
-
-
 flat_code()
 
 
