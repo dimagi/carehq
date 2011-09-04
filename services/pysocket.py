5d5bd5c5473d84ed8241d849f07d5c697b35a74f
dmyung <dmyung@dimagi.com>
2011-09-04 16:25:58 +0200

    updating some more documentation

--- services/pysocket.py
+++ services/pysocket.py
@@ -6,7 +6,7 @@ import random
 ZEBRA_SEND_TIMOUT=1500
 ZEBRA_RECEIVE_TIMEOUT=1500
 
-host = '192.168.0.85'
+host = '192.168.1.8'
 port = 9100
 
 #command = "^XA^FO10,10,^AO,30,20^FDFDTesting^S^FO10,30^BY3^BCN,100,Y,N,N^FDTesting^FS^XZ"
@@ -16,7 +16,7 @@ port = 9100
 #THIS IS THE QR CODE COMMAND
 #QR code  BQ,2,[<zoom/scale> 5 or 6, maybe 4, but 6 is the max for the 2x1],Q|M, Q is default, 
 #
-command = """
+qr_command = """
 ^XA
 ^FO20,15^BQ,2,5,,^FDMA,%(barcode_data)s^FS
 ^FO175,20^A0,30,22^FD%(last_name)s,^FS
@@ -24,7 +24,6 @@ command = """
 ^FO175,100^ABN,16,^FDSex: %(gender)s Age: %(age)s^FS
 ^FO175,120^ABN,16,^FDID: %(external_id)s^FS
 ^FO175,140^ABN,16,^FDAdmit Date: %(enroll_date)s^FS
-^PQ3,,,,
 ^XZ
 """
 
@@ -42,30 +41,34 @@ lab_command = """
 #save this to the printer as an image for dispaly. SAVES PAPER
 #^ISR:EXERPROG.GRF,N
 
+#print quantity of 3
+# ^PQ3,,,, 
+
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
 
 
 def qr_code():
     label_data = {}
-    label_data['barcode_data']=uuid.uuid1().hex
-    label_data['last_name']='Jackson'
-    label_data['first_name']='Jonathan'
+    label_data['barcode_data']="I RULE!!!"#uuid.uuid4().hex
+    label_data['last_name']='Preziosi'
+    label_data['first_name']='Mike'
     label_data['gender']='M'
-    label_data['age']='29'
+    label_data['age']='999'
     label_data['external_id']=random.randint(10000,99999)
     label_data['enroll_date']= (datetime.utcnow() - timedelta(days=random.randint(1,500))).strftime('%m/%d/%Y')
-    s.send(command % (label_data))
+    s.send(qr_command % (label_data))
     s.close()
 
 def flat_code():
     label_data = {}
-    #label_data['barcode_data']=uuid.uuid4().hex
-    print label_data['barcode_data']
-    label_data['last_name']='Jackson'
-    label_data['first_name']='Jonathan'
+    label_data['barcode_data']="I REALLY RULE!!!"#uuid.uuid4().hex
+    #print label_data['barcode_data']
+    label_data['last_name']='Preziosi'
+    label_data['first_name']='Mike'
     s.send(lab_command % (label_data))
     s.close()
-flat_code()
+#flat_code()
+qr_code()
 
 
