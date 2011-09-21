72866b40e7d25ef257a4e7f4f801a0521e9f7423
dmyung <dmyung@dimagi.com>
2011-09-21 16:38:12 -0400

    adding fixup scripts for dangling actors.

---
+++ pact_apps/pactcarehq/scripts/fix_dangling_actors.py
@@ -0,0 +1,22 @@
+from actorpermission.models.actortypes import  ProviderActor
+from permissions.models import Actor
+from tenant.models import Tenant
+
+def run():
+    all_actors = ProviderActor.view('actorpermission/all_actors', include_docs=True).all()
+
+    pact_tenant = Tenant.objects.get(name="PACT")
+    dangling = []
+
+    for actor in all_actors:
+        print "%s: %s" % (actor.doc_type, actor._id)
+        if actor.actor_uuid is None:
+            print "\tActor ID is Null"
+        try:
+            django_actor = Actor.objects.get(id=actor.actor_uuid)
+            print "\tActor exists"
+        except Actor.DoesNotExist:
+            print "\tNo django actor created"
+            actor.actor_uuid = None
+            actor.save(pact_tenant)
+    pass
