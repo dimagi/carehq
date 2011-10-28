d9f3ed59abf9f2f9e9aa9d6fbddb9523b628f969
dmyung <dmyung@dimagi.com>
2011-10-28 16:37:30 -0400

    fix prophylaxis error on new submissions, need to do migration of data to fully remedy data changes

--- shine_apps/slidesview/urls.py
+++ shine_apps/slidesview/urls.py
@@ -3,7 +3,5 @@ from django.conf import settings
 
 #(r'^projects/(?P<project_id>\d+)/?$', 'buildmanager.views.show_project'),
 urlpatterns = patterns('',
-                       (r'^slideforms$', 'slidesview.views.all_slideforms'),
-                       (r'^slideforms/(?P<doc_id>[0-9a-fA-Z]{25,32})$', 'slidesview.views.slideform'),
                        )
 
