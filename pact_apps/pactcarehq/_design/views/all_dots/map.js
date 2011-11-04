4d9cd4e3919aa30a51c5f51ddb747d6da6eb09d9
dmyung <dmyung@dimagi.com>
2011-11-04 10:10:15 -0400

    removing old code and views that aren't in use and just suck up the db indexing time

--- pact_apps/pactcarehq/_design/views/all_dots/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) {
-    //view to see all patients that are DOT patients.
-    if (doc.base_type == "BasePatient" && doc.arm == "DOT")
-        emit(doc._id, null); 
-}
--- pact_apps/pactcarehq/_design/views/last_progress_note/map.js
+++
@@ -1,5 +0,0 @@
-function(doc) {
-    if (doc.doc_type == "XFormInstance" && doc.xmlns == "http://dev.commcarehq.org/pact/progress_note" && doc.form.Meta !== undefined) {
-        emit(doc.form.note.pact_id, [doc._id, doc.form.note.encounter_date]);
-    }
-}
