200c81cad6bd0c3fd6cbb91b8d332d834be23d77
Christian Ternus <ternus@mit.edu>
2012-01-10 09:59:19 -0500

    Deploy settings (incomplete)

---
+++ deploy-settings.py
@@ -0,0 +1,96 @@
+"""
+House the settings for your modules, and your project in general, here.
+"""
+
+"""The remote hosts you'll be deploying this project to."""
+#STAGING_HOST = ["50.56.246.140"]
+#STAGING_HOST = ["192.168.100.62"]
+PRODUCTION_HOST = ["192.168.100.108"]
+
+
+#The unix user that will be the owner of this project
+PROJECT_USER = "cternus"
+SUDO_USER = "ashand"
+
+PROJECT_NAME = "ashand"
+
+
+#Path to folder that holds both the code_root and virtualenv folder.
+#PACKAGES_PROJECT_ROOT gets passed through the fabric 'env' dict, so you
+#can use keywords from there to construct this path.
+#e.g. '/home/some_name/www/%(environment)/'
+#
+#some other keyword options:
+#%(project_user),
+#%(os),
+#%(environment) - The 'deploy level' e.g. 'staging' or 'production'
+PROJECT_ROOT = '/home/ashand/'
+
+#Deploy modules we'll be utilizing in this project
+MODULES = [#"modules.os",
+           "modules.db",
+           "modules.web",
+           "modules.utils",
+           "modules.packages",
+           "modules.django",
+           "modules.supervisor",
+           ]
+
+
+
+##### OS MODULE SPECIFIC SETTINGS ######
+#OS_PACKAGE_LIST_PATH_REDHAT = "../../../yum-packages.txt" #each package on a new line in this file
+
+##### PACKAGES MODULE SPECIFIC SETTINGS ######
+PACKAGES_PIP_REQUIREMENTS_PATH = "apps.txt" #The path to pip_requires file ON THE LOCAL Machine, relative to this settings.py file.
+
+
+
+##### DJANGO MODULE SPECIFIC SETTINGS ######
+DJANGO_GIT_REPO_URL = "git://github.com/dimagi/carehq.git" #django project repo url
+DJANGO_STAGING_GIT_BRANCH = "master"
+DJANGO_STAGING_SERVER_NAME = "staging.some_project.com"
+DJANGO_PRODUCTION_GIT_BRANCH = "ashandui-ticketing"
+DJANGO_PRODUCTION_SERVER_NAME = "ashand.dimagi.com"
+DJANGO_GUNICORN_PORT = '9010'
+DJANGO_LOCALSETTINGS_LOCAL_PATH = "localsettings.py" #Path to localsettings on local machine
+#A dictionary you can use if you want to treat localsettings as a template.
+#This dict get's autopopulated with other useful keys:
+#* project_root
+#* www_root - holds the log_dir and code_root
+#* log_dir
+#* code_root - where your django code ends up
+#* project_media - django media folder (in your code_root by default) ((usually used by collectstatic))
+#* project_static - django static files folder location, also in code_root by default
+#* virtualenv_root
+#* services - path where things like apache.conf and supervisor.conf end up.
+DJANGO_LOCALSETTINGS_TEMPLATE_DICT = {}
+DJANGO_LOCALSETTINGS_NO_TEMPLATE = True #Set to false if you want to treat localsettings.py as a template
+DJANGO_LOCALSETTINGS_REMOTE_DESTINATION = "ashand/localsettings.py" #RELATIVE TO THE CODE_ROOT ON REMOTE MACHINE
+
+##### WEB MODULE SPECIFIC SETTINGS ######
+WEB_HTTPD = "apache" #apache2 or nginx
+WEB_CONFIG_TEMPLATE_PATH = "..\\..\\workspace-temp\\aremind\\services\\templates\\apache.conf"
+WEB_PARAM_DICT = {
+    "HOST_PORT" : 80 #Primary port for hosting things
+}
+##### SUPERVISOR MODULE SPECIFIC SETTINGS ######
+SUPERVISOR_TEMPLATE_PATH = "..\\..\\workspace-temp\\aremind\\services\\templates\\supervisor.conf"  #PATH ON LOCAL DISK, RELATIVE TO THIS FILE
+SUPERVISOR_INIT_TEMPLATE = "templates\\supervisor-init"  #PATH ON LOCAL DISK, Relative to THIS file.
+SUPERVISOR_DICT = {
+    "gunicorn_port": DJANGO_GUNICORN_PORT,
+    "gunicorn_workers": 3,
+    "project": PROJECT_NAME,
+    "server_port": DJANGO_GUNICORN_PORT,
+}
+
+######## DB MODULE SETTINGS #####################
+DB_DATABASE_NAME = "ashand" #name of database to be created
+DB_DATABASE_USER = "ashand" #owner of new database
+DB_ALTERNATE_LOCATION = "/opt/data/" #Location on remote machine where you would like to store the DB
+DB_MOVE_DB_AT_BOOTSTRAP = True #(default is False). If this is set to true, DB will be relocated according to DB_ALTERNATE_LOCATION
+
+
+##### UTILS MODULE SPECIFIC SETTINGS ######
+#some stuff...
+
