87864708da821302b2d0dfcc99fb65f27d5ba5b3
dmyung <dmyung@dimagi.com>
2012-01-10 13:56:10 -0500

    updating documentation on ccdreceiver

--- ashand_apps/carehqapp/views/ccdreceiver.py
+++ ashand_apps/carehqapp/views/ccdreceiver.py
@@ -1,3 +1,4 @@
+import uuid
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,13 +9,19 @@ from receiver.util import spoof_submission
 @require_POST
 @csrf_exempt
 def receive_ccd(request):
-    #totally ghetto, but it works
-    resp = spoof_submission(reverse('receiver.views.post'), request.POST['patientsessiondata'], hqsubmission=False)
+    """
+    Receive a CCD from the CI push services as per section 14.4 example.
+    Returns a 200 response xml fragment.
+    """
+    #Commented out until couchforms receiver is updated
     #def post_callback(doc):
-        #response_text = """<Response><Code>SUCCESS</Code><Message>doc submitted id:%s</Message></Response>""" % doc['_id']
-        #return HttpResponse(response_text)
-    #return couchforms_post(request, post_callback)
-    response_text = """<Response><Code>SUCCESS</Code></Response>"""
     #response_text = """<Response><Code>SUCCESS</Code><Message>doc submitted id:%s</Message></Response>""" % doc['_id']
-    return HttpResponse(response_text)
+    #return HttpResponse(response_text)
+    #return couchforms_post(request, post_callback)
+    #response_text = """<Response><Code>SUCCESS</Code><Message>doc submitted id:%s</Message></Response>""" % doc['_id']
+
+    #Temporary method for submission until receiver logic is updated to handle patientsessiondata POST key
+    resp = spoof_submission(reverse('receiver.views.post'), request.POST['patientsessiondata'], hqsubmission=False)
+    response_text = "<?xml version=\"1.0\" encoding=\"utf-8\"?><Response><Code>SUCCESS</Code><Message>Thank you %s</Message></Response>" % uuid.uuid4().hex
+    return HttpResponse(response_text) #default this assumes is a 200 response code.
 
