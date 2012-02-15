2c917201b74231363346be5af4940ad88f49cfc1
dmyung <dmyung@dimagi.com>
2012-02-15 11:17:19 -0500

    fixing out hcms sync output

--- ashand_apps/carehqapp/management/commands/hcms_patient_sync.py
+++ ashand_apps/carehqapp/management/commands/hcms_patient_sync.py
@@ -42,7 +42,8 @@ class Command(BaseCommand):
         ret_arr = []
         for dj_patient in django_patients:
             couchdoc = dj_patient.couchdoc
-            if couchdoc._id == 'c6f12e6c459c45b9b0e7272d1b23f932' or couchdoc._id == '157e9fae3800489b9dcb533cb1d48f66':
+            if couchdoc._id == 'c6f12e6c459c45b9b0e7272d1b23f932' or couchdoc._id == '157e9fae3800489b9dcb533cb1d48f66' or couchdoc._id == '2269122d211c472eb904ee2c4cf1ddf9'\
+                or couchdoc._id == 'f21a992108fe4874ad5d0a8d38ef7847' or couchdoc._id == '36264411fa4643f4a2675742170b447c':
                 continue
             pt_json = couchdoc.to_json()
 
@@ -51,25 +52,25 @@ class Command(BaseCommand):
             ret_json['DocID'] = base64.b64encode(uuid.UUID(doc_id).bytes)
             ret_json['ExternalUserID'] = pt_json['study_id']
             ret_json['FirstName'] = pt_json['first_name']
-            ret_json['LastName'] = pt_json['last_name']
+            ret_json['LastName'] = "SUBJECT" #pt_json['last_name']
             ret_json['Gender'] = pt_json['gender']
 
             if len(pt_json['address']) > 0:
                 address = pt_json['address'][0]
 
-                ret_json['Address1'] = address['street']
-                ret_json['City'] = address['city']
-                ret_json['State'] = address['state']
-                ret_json['PostalCode'] = address['postal_code']
+                ret_json['Address1'] = "585 Massachusetts Avenue Suite 3" #address['street']
+                ret_json['City'] = "Cambridge" #address['city']
+                ret_json['State'] = "MA" #address['state']
+                ret_json['PostalCode'] = "02139" #address['postal_code']
             else:
-                ret_json['Address1'] = ''
-                ret_json['City'] = ''
-                ret_json['State'] = ''
-                ret_json['PostalCode'] = ''
+                ret_json['Address1'] ="585 Massachusetts Avenue Suite 3"
+                ret_json['City'] = 'Cambridge'
+                ret_json['State'] = 'MA'
+                ret_json['PostalCode'] = '02139'
             if len(pt_json['phones']) > 0:
-                ret_json['PhoneNumber'] = pt_json['phones'][0]['number']
+                ret_json['PhoneNumber'] = '617-649-2214' #pt_json['phones'][0]['number']
             else:
-                ret_json['PhoneNumber'] = ''
+                ret_json['PhoneNumber'] = '617-649-2214'
             ret_arr.append(ret_json)
         print simplejson.dumps(ret_arr)
 
