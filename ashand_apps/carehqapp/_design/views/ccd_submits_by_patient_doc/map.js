ccbc76fcd24088479be7c27a6894fb40cf3451f6
dmyung <dmyung@dimagi.com>
2012-03-08 16:09:20 -0500

    fixup parseint for date checks

--- ashand_apps/carehqapp/_design/views/ccd_submits_by_patient_doc/map.js
+++ ashand_apps/carehqapp/_design/views/ccd_submits_by_patient_doc/map.js
@@ -3,9 +3,9 @@ function (doc) {
         if (doc.xmlns == 'urn:hl7-org:v3') {
             //datetime.strptime(submission.form['author']['time']['@value'][0:8], '%Y%m%d'
             var timestring = doc.form['author']['time']['@value'];
-            var year = parseInt(timestring.substring(0,4));
-            var month = parseInt(timestring.substring(4,6));
-            var day = parseInt(timestring.substring(6,8));
+            var year = parseInt(timestring.substring(0,4), 10);
+            var month = parseInt(timestring.substring(4,6), 10);
+            var day = parseInt(timestring.substring(6,8), 10);
 
             emit([doc.form['recordTarget']['patientRole']['id'][1]['@extension'], year, month, day], null);
         }
