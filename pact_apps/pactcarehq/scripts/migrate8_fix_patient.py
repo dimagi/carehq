8211e73711af8d532346c869c9451d6eeaffcd40
dmyung <dmyung@dimagi.com>
2012-03-15 00:57:15 -0400

    removing saves from migrate script as it has already been run

--- pact_apps/pactcarehq/scripts/migrate8_fix_patient.py
+++ pact_apps/pactcarehq/scripts/migrate8_fix_patient.py
@@ -10,8 +10,8 @@ def run():
     xforms = XFormInstance.view('pactcarehq/all_submits_by_patient_date', startkey=['493',0], endkey=['493',9999], include_docs=True).all()
 
     for x in xforms:
-        x['form']['pact_id'] = '199'
-        x.save()
+        #x['form']['pact_id'] = '199'
+        #x.save()
         print "flipping form's pact_id from 493->199"
 
 
@@ -19,15 +19,15 @@ def run():
     patient_doc = django_patient.couchdoc
     if patient_doc.pact_id =='493':
         print "flipping patient 493->199"
-        patient_doc.pact_id='199'
-        patient_doc.save()
+        #patient_doc.pact_id='199'
+        #patient_doc.save()
 
     case_id = patient_doc.case_id
 
     case = CommCareCase.get(case_id)
     if case.external_id == '493':
-        case.external_id = '199'
-        case.save()
+        #case.external_id = '199'
+        #case.save()
         print "flipping case 493->199"
 
 
